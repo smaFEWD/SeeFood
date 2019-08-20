7ed4014075fb6cf33a94a6e48d815c6607ab1e1e
Sandi Ma <sandima.job@gmail.com>
2019-08-19 23:36:13 -0700

    coded up UIPickerController to access camera and photoLibrary, and added the data Model

--- SeeFood/SeeFood/ViewController.swift
+++ SeeFood/SeeFood/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary // implementing camera functionality in app
+        imagePicker.allowsEditing = false
+        
+    }
+    // this tells the delegate that the user has picked an image or movie
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // this key will yield the image picked by the user
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+    }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
